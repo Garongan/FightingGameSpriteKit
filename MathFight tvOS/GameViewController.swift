1c68bec2551ae029ef3d38ca3f85e18e45e8bc4b
Garongan <alvindo56@gmail.com>
2025-07-31 20:38:40 +0700

    Initial Commit

---
+++ MathFight tvOS/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  MathFight tvOS
+//
+//  Created by Alvindo Tri Jatmiko on 31/07/25.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
